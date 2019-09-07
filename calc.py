9204bf886f6f71d1b3f656f5490bd2ecf5ebc319
Roman <namor925@gmail.com>
2019-09-07 19:34:17 +0700

    correct calculate from latin

--- calc.py
+++ calc.py
@@ -3,17 +3,41 @@
 #
 
 import sys
+import logging
 
+# define basic variables
 list_operators = ['+', '-', '/', '*']
-latin_digit = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'] # если не большие!
-correllation_table = {'I': 1, 'II': 2, 'III': 3, 'IV': 4, 'V': 5, 
-'VI': 6,}
+latin_digit = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X']  # TODOS! IF little chars!
+arab_digit = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def lat_to_arabic():
+    lat_to_ar = {}
+    ar_to_lat = {}
+    i2 = 0
+    for i in latin_digit:
+        lat_to_ar[i] = arab_digit[i2]
+        i2 = i2 + 1
+
+    for i in arab_digit:
+        pass
+    return lat_to_ar
+
+
+def check_operans_one_group(operand1,  operand2):
+    ''' if any operand is misc type - return False'''
+    if operand1 in latin_digit and operand2 in latin_digit:
+        return True
+    elif operand1 in arab_digit and operand2 in arab_digit:
+        return True
+    else:
+        return False
 
 
 def check_operand_arabic_digit(operand):
     '''check - operand is arabic digit?'''
     if operand.isdigit():
-        return int(operand)
+        return True
     else:
         return False
 
@@ -35,8 +59,10 @@ def checker_operator(operator):
 
 def check_arguments_count():
     if len(sys.argv) != 3:
+        #print('ok, 3')
         return False
     else:
+        #print('error')
         return True
 
 
@@ -48,6 +74,33 @@ def calc(operand1, operator, operand2):
 
 
 if __name__ == "__main__":
-    print(sys.argv[1])
-    tmp = calc(3, '+', 2)
-    print(tmp)
+    a = sys.argv[1]
+    b = sys.argv[3]
+    check_arguments_count() # элементов три!
+    if checker_operator(sys.argv[2]) is True:  # я +-*/
+        pass
+    else:
+        print('operator error')
+    #первая цифра десятичная или римская
+    if check_operand_latin_digit(a) is True or check_operand_arabic_digit(a) is True:
+        pass
+    else:
+        print('не буду нихачу!')
+        exit()
+    #вторая цифра десятичная или римская
+    if check_operand_latin_digit(b) is True or check_operand_arabic_digit(b) is True:
+        pass
+    else:
+        print('не буду нихачу! второй не цифирь!')
+        exit()
+    #print(a, b)
+    lat_ar = lat_to_arabic()
+    equal = check_operans_one_group(int(a), int(b))
+    if equal is True:
+        pass
+    else:
+        print('бля, еррор!')
+        exit()
+    res = calc(a,  sys.argv[2],  b)
+    print(res)
+    
